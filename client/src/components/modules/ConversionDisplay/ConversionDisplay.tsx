f063af233e511374f54d202d3e4da3cef6a72838
Gerry Saporito <gerrysaporito@gmail.com>
2022-06-18 14:17:02 -0400

    Added tailwind

---
+++ client/src/components/modules/ConversionDisplay/ConversionDisplay.tsx
@@ -0,0 +1,5 @@
+interface IConversionDisplay {}
+
+export const ConversionDisplay: React.FC<IConversionDisplay> = () => {
+  return <div id="CONVERSION_DISPLAY"></div>;
+};
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import React from 'react';
+import { ChakraProvider, createStandaloneToast } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const { toast, ToastContainer } = createStandaloneToast();
+
+export const standaloneToast = toast;
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
