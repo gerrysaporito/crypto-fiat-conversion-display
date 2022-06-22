938e4947bdeabd203c70c59b09fe8facd02541d1
Gerry Saporito <gerrysaporito@gmail.com>
2022-06-22 00:01:47 -0400

    Created cleaning funciton and tests to sanitize numerical inputs

---
+++ client/src/utils/utility/Cleaning.ts
@@ -0,0 +1,24 @@
+export class Cleaning {
+  constructor() {}
+
+  static cleanStringAmount = (amount: string): string => {
+    const _amount = parseFloat(amount).toString();
+    const whole = _amount.split('.')[0];
+    const decimals = _amount.split('.')[1];
+
+    // Only whole numbers
+    if (!decimals || whole.length >= 5) return whole;
+
+    // Ensure a min of 2 decimal places if decimals present
+    if (decimals.length === 1) return `${whole}.${decimals}0`;
+
+    // Reduce decimals to ensure that number accounts for less than 6 digits
+    if (whole.length + decimals.length >= 6)
+      return `${whole}.${decimals.slice(0, 6 - whole.length)}`;
+
+    // Ensure whole number and decimal accounts for less than 6 digits
+    if (decimals && decimals.length > 0) return `${whole}.${decimals}`;
+
+    return '-1';
+  };
+}
---
+++ client/src/utils/utility/__test__/Cleaning.test.ts
@@ -0,0 +1,22 @@
+import { Cleaning } from '../Cleaning';
+
+/*
+ * To use the mock stripe, remove the .old extension on the stripe.ts file in the __mocks__ folder.
+ */
+describe(`Tests Cleaning utility class' static functions`, () => {
+  it('tests cleanStringAmount edge cases', async () => {
+    // Check no decimal starting with non-zero number
+    expect(Cleaning.cleanStringAmount('100')).toEqual('100');
+    expect(Cleaning.cleanStringAmount('0100')).toEqual('100');
+    expect(Cleaning.cleanStringAmount('000000100')).toEqual('100');
+    // Check decimal starting with zero
+    expect(Cleaning.cleanStringAmount('0.00123')).toEqual('0.00123');
+    expect(Cleaning.cleanStringAmount('00.00123')).toEqual('0.00123');
+    expect(Cleaning.cleanStringAmount('000000.00123')).toEqual('0.00123');
+    // Check whole number with decimal places
+    expect(Cleaning.cleanStringAmount('100.0')).toEqual('100');
+    expect(Cleaning.cleanStringAmount('002.00')).toEqual('2');
+    expect(Cleaning.cleanStringAmount('002.01')).toEqual('2.01');
+    expect(Cleaning.cleanStringAmount('002.1')).toEqual('2.10');
+  });
+});
